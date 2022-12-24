3d9553450375f8d320f03473180b7c6749d10e19
Malik <albakrimalik@gmail.com>
2022-12-24 15:25:37 -0800

    few style updates

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
+import { useState } from 'react';
 
 const Home = () => {
+  const [userInput, setUserInput] = useState('');
+  const onUserChangedText = (event) => {
+    console.log(event.target.value);
+    setUserInput(event.target.value);
+  };
+
   return (
     <div className="root">
       <Head>
@@ -11,11 +18,21 @@ const Home = () => {
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Aya & Hadith Finder</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>Learn new ayas/hadiths about a certain subject, find the verse at the tip of your tounge</h2>
           </div>
+        </div>
+        <div className="prompt-container">
+          <textarea placeholder="Islamic topic or describe an ayah/hadith. Ex: Ayah about pork being haram." className="prompt-box" value={userInput} onChange={onUserChangedText}/>
+          <div className="prompt-buttons">
+    <a className="generate-button" onClick={null}>
+      <div className="generate">
+        <p>Find</p>
+      </div>
+    </a>
+  </div>
         </div>
       </div>
       <div className="badge-container grow">
@@ -33,5 +50,6 @@ const Home = () => {
     </div>
   );
 };
+
 
 export default Home;
